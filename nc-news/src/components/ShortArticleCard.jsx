7a0c3af46fd2373f4cc06e249affe618b1b0b577
Tony Lancaster <lanc1968@gmail.com>
2023-03-13 15:22:38 +0000

    Minor CSS fix.

--- nc-news/src/components/ShortArticleCard.jsx
+++ nc-news/src/components/ShortArticleCard.jsx
@@ -3,7 +3,7 @@ import Card from "react-bootstrap/Card";
 const ShortArticleCard = ({ article }) => {
   return (
     <li className="mb-4">
-      <Card className="w-100">
+      <Card className="w-100 flex-row flex-wrap">
         <Card.Img variant="top" src={article.article_img_url} className="w-25" />
         <Card.Body className="w-75">
           <Card.Title className="pb-2"><h3>{article.title}</h3></Card.Title>
