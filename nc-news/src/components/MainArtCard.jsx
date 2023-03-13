274488b7040345ad06a8c051cd7128bc056eed8b
Tony Lancaster <lanc1968@gmail.com>
2023-03-13 16:42:18 +0000

    Add author & posted date

--- nc-news/src/components/MainArtCard.jsx
+++ nc-news/src/components/MainArtCard.jsx
@@ -22,15 +22,28 @@ const MainArtCard = () => {
   return (
     <section>
       <Card className="w-100">
-        <Card.Title className="my-4 ms-4">{article.title}</Card.Title>
+        <Card.Title className="my-4 ms-4">
+          <h4 className="font-weight-bold">{article.title}</h4>
+        </Card.Title>
         <Card.Img variant="top" src={article.article_img_url} />
         <Card.Body>
           <Card.Text>
             Really need some text for the body, rather than letting the title
             initiate discussion
           </Card.Text>
+          <Card.Text className="font-weight-bold d-flex flex-row justify-content-between">
+            <p>
+              {" "}
+              Author:{" "}
+              <span className="font-weight-normal">{article.author}</span>
+            </p>{" "}
+            <p>
+              Posted: <span className="font-weight-normal"></span>
+              {article.created_at}
+            </p>
+          </Card.Text>
         </Card.Body>
-        <Card.Footer className=" d-flex flex-row justify-content-around align-items-center">
+        <Card.Footer className=" d-flex flex-row justify-content-around align-items-center ">
           <p className="m-0">Votes: {article.votes}</p>
           <p className="m-0">Comments: {article.comment_count} </p>
         </Card.Footer>
