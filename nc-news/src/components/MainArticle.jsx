8ca481b83b2827255988fd83669c5113cfcbbcec
Tony Lancaster <lanc1968@gmail.com>
2023-03-18 14:13:30 +0000

    Add prop to pass total comment_count from article loader down to... comments list & paginator.

--- nc-news/src/components/MainArticle.jsx
+++ nc-news/src/components/MainArticle.jsx
@@ -1,9 +1,13 @@
 import { useParams } from "react-router-dom";
 import MainArtCard from "./MainArtCard";
 import CommentsList from './CommentsList';
+import { useState } from "react";
 
 
 const MainArticle = () => {
+
+  const [commentTotalCount, setCommentTotalCount] = useState(null);
+
   const { article_id } = useParams();
   
 /*   if (isNaN(article_id)) {
@@ -11,10 +15,15 @@ const MainArticle = () => {
   } */
     return (
       <section className="article-section">
-        <MainArtCard article_id={article_id} />
-        
+        <MainArtCard
+          article_id={article_id}
+          setCommentTotalCount={setCommentTotalCount}
+        />
 
-        <CommentsList article_id={article_id} />
+        <CommentsList
+          article_id={article_id}
+          commentTotalCount={commentTotalCount}
+        />
       </section>
     );
 };
