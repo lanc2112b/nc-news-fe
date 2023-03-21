0bce55c18bb5f4dcdc1fd78382e4b60b89f0cd23
Tony Lancaster <lanc1968@gmail.com>
2023-03-21 22:25:22 +0000

    Add footer to layout. Remove react strict mode.

--- nc-news/src/components/Footer.jsx
+++ nc-news/src/components/Footer.jsx
@@ -1,11 +1,10 @@
-import { Row } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 const Footer = () => {
     return (
-        <footer className="w-100">
-            <Row>
-               <p className="col8 offset-2"> Footer section here.</p>
-            </Row>
-        
+      <footer className="w-100 footer mt-auto">
+        <Container className="h-100 pt-5 text-center">
+          <p>NC-News 2023</p>
+        </Container>
       </footer>
     );
 
