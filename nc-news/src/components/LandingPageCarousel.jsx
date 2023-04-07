9c3983696ea3b91dc99cc988bedd18aaa0774c9e
Tony Lancaster <lanc1968@gmail.com>
2023-04-07 19:53:16 +0100

    Updated carousel slides.

--- nc-news/src/components/LandingPageCarousel.jsx
+++ nc-news/src/components/LandingPageCarousel.jsx
@@ -13,114 +13,54 @@ const LandingPageCarousel = () => {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="carousel_imgs/holly_and_pebbles.jpg"
-          alt="Holly & Pebbles, the dogs, running through snow."
+          src="carousel_imgs/node_slide.png"
+          alt="Node.js slide"
         />
 
         <Carousel.Caption>
-          <h3>Holly & Pebbles</h3>
-          <p>The terrible twins!</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="carousel_imgs/mrmush.jpg"
-          alt="Another dog, Mr Mush, on a beach."
-        />
-
-        <Carousel.Caption>
-          <h3>Mr Mush</h3>
-          <p>Trouble maker extraordinaire</p>
+          <h3>Back End</h3>
+          <p>Node.js, the API provider</p>
         </Carousel.Caption>
       </Carousel.Item>
 
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="carousel_imgs/brawndo.jpg"
-          alt="Brawndo, HAS WHAT PLANTS CRAVE, sorry bit random."
+          src="carousel_imgs/postgres_slide.png"
+          alt="Postgresql"
         />
+
         <Carousel.Caption>
-          <h3>Brawndo</h3>
-          <p>HAS WHAT PLANTS CRAVE!</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="carousel_imgs/whitby.jpg"
-          alt="Whitby South pier, long exposure"
-        />
-        <Carousel.Caption>
-          <h3>Whitby</h3>
-          <p>Has vampires & chips!</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="carousel_imgs/talacre.jpg"
-          alt="Talacre lighthouse, long exposure"
-        />
-        <Carousel.Caption>
-          <h3>Talacre</h3>
-          <p>Leaning lighthouse</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="carousel_imgs/poppyfields.jpg"
-          alt="Field of poppies at dusk, long exposure"
-        />
-        <Carousel.Caption>
-          <h3>Poppy Fields</h3>
-          <p>You've got red on you!</p>
+          <h3>Back End</h3>
+          <p>PostgreSQL, the database</p>
         </Carousel.Caption>
       </Carousel.Item>
 
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="carousel_imgs/lincolnshire.jpg"
-          alt="Powerstation near lincoln with ominously dark clouds"
+          src="carousel_imgs/react_slide.png"
+          alt="React.js"
         />
         <Carousel.Caption>
-          <h3>Dark Skies</h3>
-          <p>The mid 90's SciFi series!</p>
+          <h3>Front End</h3>
+          <p>React, the user interface builder</p>
         </Carousel.Caption>
       </Carousel.Item>
-
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="carousel_imgs/windmills.jpg"
-          alt="Wind generation turbines at dusk"
+          src="carousel_imgs/bootstrap_slide.png"
+          alt="Bootstrap 5"
         />
         <Carousel.Caption>
-          <h3>Windmills</h3>
-          <p>
-            Round like a circle in a spiral, like a wheel within a wheel
-            <br /> Never ending or beginning on an ever spinning reel
-          </p>
+          <h3>Front End</h3>
+          <p>Bootstrap, components, columns & more</p>
         </Carousel.Caption>
       </Carousel.Item>
+      
 
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="carousel_imgs/filey.jpg"
-          alt="View north looking out to sea, clouds and red sky, from Filey promenade"
-        />
-        <Carousel.Caption>
-          <h3>Sky's On Fire</h3>
-          <p>
-            The World's On Fire The World's On Fire
-            <br /> And it's too close to the wire
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
+      
     </Carousel>
   );
 }
